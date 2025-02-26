3d149bce3e0312b3b52e089f127d83f0e4218b76
Andre Anjos <andre.dos.anjos@gmail.com>
2025-02-26 19:27:13 +0100

    Try fix reported in https://github.com/google/go-licenses/issues/128

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X main.version=${PKG_VERSION}"
-go-licenses save . --save_path=license-files --ignore github.com/mattn/go-localereader
+go-licenses save --ignore github.com/mattn/go-localereader --save_path=license-files .
 
 mkdir -p ${PREFIX}/etc/bash_completion.d
 mkdir -p ${PREFIX}/share/fish/vendor_completions.d
