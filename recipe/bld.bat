de0362e4a3da9cfc030b203d805fb1f870d48c46
Andre Anjos <andre.dos.anjos@gmail.com>
2025-02-28 08:25:35 +0100

    Remove use of --ignore

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="-s -X main.version=%PKG_VERSION%" || goto :error
-go-licenses save --ignore github.com/mattn/go-localereader --save_path=license-files . || goto :error
+go-licenses save --save_path=license-files . || goto :error
 
 goto :EOF
 
