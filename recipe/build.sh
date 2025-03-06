7fc05529e2a209c65fa5b38cae86d5499c293426
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-06 10:54:04 -0800

    set GOTOOLCHAIN

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+go env -w GOTOOLCHAIN=local
 go build -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X main.version=${PKG_VERSION}"
 go-licenses save --save_path=license-files .
 
